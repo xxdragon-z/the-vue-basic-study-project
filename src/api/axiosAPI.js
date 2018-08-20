d242939ea90fd48726da2e1ae13a5c33160661fc
xiongxiaolong <xiongxiaolong@bjzkar.com>
2018-08-20 13:37:13 +0800

    添加post，delete，put，patch等查询方法

--- src/api/axiosAPI.js
+++ src/api/axiosAPI.js
@@ -5,10 +5,25 @@ export const getRequest = (url, params) => {
   console.info("开始调用：url=" + url)
   if (url) {
     if (!params)
-      params={"msg":null};
-    return axios.get(url,{params:params}).then(result => result.data)
+      params = {"msg": null};
+    return axios.get(url, {params: params}).then(result => result.data)
   }
-
 }
+export const PUT = (url, params) => {
+  return axios.put(url, params).then(res => res.data)
+}
+
+export const DELETE = (url, params) => {
+  return axios.delete(url, {params: params}).then(res => res.data)
+}
+
+export const PATCH = (url, params) => {
+  return axios.patch(url, params).then(res => res.data)
+}
+export const POST = (url, params) => {
+  console.info(url + '++++++++' + params)
+  return axios.post(url, params).then(res => res.data)
+}
+
 
 
